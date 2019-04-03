58c66bdafacd9a18f37894feb56ce346d430d4be
Martín M <skydiver@users.noreply.github.com>
2019-04-02 22:16:20 -0300

    layout + navbar

---
+++ layouts/Nav/Nav.vue
@@ -0,0 +1,102 @@
+<template>
+  <nav
+    class="bg-black pt-2 md:pt-1 pb-1 px-1 mt-0 h-auto fixed w-full z-20 pin-t"
+  >
+    <div class="flex flex-wrap items-center">
+      <div
+        class="flex flex-shrink md:w-1/3 justify-center md:justify-start text-white"
+      >
+        <a href="#">
+          <span class="text-xl pl-2">nuxt-tailwind-dashboard</span>
+        </a>
+      </div>
+
+      <div
+        class="flex flex-1 md:w-1/3 justify-center md:justify-start text-white px-2"
+      >
+        <SearchBar />
+      </div>
+
+      <div
+        class="flex w-full pt-2 content-center justify-between md:w-1/3 md:justify-end"
+      >
+        <ul
+          class="list-reset flex justify-between flex-1 md:flex-none items-center"
+        >
+          <li class="flex-1 md:flex-none md:mr-3">
+            <a class="inline-block py-2 px-4 text-white no-underline" href="#"
+              >Active</a
+            >
+          </li>
+          <li class="flex-1 md:flex-none md:mr-3">
+            <a
+              class="inline-block text-grey-dark no-underline hover:text-grey-lighter hover:text-underline py-2 px-4"
+              href="#"
+              >link</a
+            >
+          </li>
+          <li class="flex-1 md:flex-none md:mr-3">
+            <div class="relative inline-block">
+              <button
+                onclick="toggleDD('myDropdown')"
+                class="drop-button text-white focus:outline-none"
+              >
+                <span class="pr-2"><i class="em em-robot_face"></i></span> Hi,
+                User
+                <svg
+                  class="h-3 fill-current"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                >
+                  <path
+                    d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"
+                  />
+                </svg>
+              </button>
+              <div
+                id="myDropdown"
+                class="dropdownlist absolute bg-black text-white pin-r mt-3 p-3 overflow-auto z-30 invisible"
+              >
+                <input
+                  id="myInput"
+                  type="text"
+                  class="drop-search p-2 text-grey-dark"
+                  placeholder="Search.."
+                  onkeyup="filterDD('myDropdown','myInput')"
+                />
+                <a
+                  href="#"
+                  class="p-2 hover:bg-grey-darkest text-white text-sm no-underline hover:no-underline block"
+                  ><i class="fa fa-user fa-fw"></i> Profile</a
+                >
+                <a
+                  href="#"
+                  class="p-2 hover:bg-grey-darkest text-white text-sm no-underline hover:no-underline block"
+                  ><i class="fa fa-cog fa-fw"></i> Settings</a
+                >
+                <div class="border border-grey-darkest"></div>
+                <a
+                  href="#"
+                  class="p-2 hover:bg-grey-darkest text-white text-sm no-underline hover:no-underline block"
+                  ><i class="fas fa-sign-out-alt fa-fw"></i> Log Out</a
+                >
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+import SearchBar from '~/layouts/Nav/SearchBar';
+
+export default {
+  components: {
+    SearchBar
+  }
+};
+</script>
+
+<style scoped></style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,22 @@
 <template>
   <div>
+    <Nav />
     <nuxt />
   </div>
 </template>
+
+<script>
+import Nav from '~/layouts/Nav';
+
+export default {
+  components: {
+    Nav
+  },
+  head: {
+    title: 'Nuxt.js Dashboard Starter',
+    bodyAttrs: {
+      class: 'bg-black font-sans leading-normal tracking-normal mt-12'
+    }
+  }
+};
+</script>
