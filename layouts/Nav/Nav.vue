cce8cb05b12e0a6de780adc6ea2a838ee3b0ec75
Martín M <skydiver@users.noreply.github.com>
2019-04-02 22:34:51 -0300

    refactoring navbar

--- layouts/Nav/Nav.vue
+++ layouts/Nav/Nav.vue
@@ -6,8 +6,11 @@
       <div
         class="flex flex-shrink md:w-1/3 justify-center md:justify-start text-white"
       >
-        <a href="#">
-          <span class="text-xl pl-2">nuxt-tailwind-dashboard</span>
+        <a class="no-underline" href="#">
+          <span
+            class="text-xl pl-2 text-grey-lighter antialiased font-bold subpixel-antialiased"
+            >nuxt-tailwind-dashboard</span
+          >
         </a>
       </div>
 
@@ -36,52 +39,7 @@
             >
           </li>
           <li class="flex-1 md:flex-none md:mr-3">
-            <div class="relative inline-block">
-              <button
-                onclick="toggleDD('myDropdown')"
-                class="drop-button text-white focus:outline-none"
-              >
-                <span class="pr-2"><i class="em em-robot_face"></i></span> Hi,
-                User
-                <svg
-                  class="h-3 fill-current"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                >
-                  <path
-                    d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"
-                  />
-                </svg>
-              </button>
-              <div
-                id="myDropdown"
-                class="dropdownlist absolute bg-black text-white pin-r mt-3 p-3 overflow-auto z-30 invisible"
-              >
-                <input
-                  id="myInput"
-                  type="text"
-                  class="drop-search p-2 text-grey-dark"
-                  placeholder="Search.."
-                  onkeyup="filterDD('myDropdown','myInput')"
-                />
-                <a
-                  href="#"
-                  class="p-2 hover:bg-grey-darkest text-white text-sm no-underline hover:no-underline block"
-                  ><i class="fa fa-user fa-fw"></i> Profile</a
-                >
-                <a
-                  href="#"
-                  class="p-2 hover:bg-grey-darkest text-white text-sm no-underline hover:no-underline block"
-                  ><i class="fa fa-cog fa-fw"></i> Settings</a
-                >
-                <div class="border border-grey-darkest"></div>
-                <a
-                  href="#"
-                  class="p-2 hover:bg-grey-darkest text-white text-sm no-underline hover:no-underline block"
-                  ><i class="fas fa-sign-out-alt fa-fw"></i> Log Out</a
-                >
-              </div>
-            </div>
+            <UserMenu />
           </li>
         </ul>
       </div>
@@ -91,10 +49,12 @@
 
 <script>
 import SearchBar from '~/layouts/Nav/SearchBar';
+import UserMenu from '~/layouts/Nav/UserMenu';
 
 export default {
   components: {
-    SearchBar
+    SearchBar,
+    UserMenu
   }
 };
 </script>
---
+++ layouts/Nav/UserMenu.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="relative inline-block">
+    <button
+      class="drop-button text-white focus:outline-none"
+      @click.prevent="toggleDD('userDropDown')"
+    >
+      <span class="pr-2">
+        <img
+          class="w-6 h-6 rounded-full"
+          src="http://i.pravatar.cc/100"
+          alt="Avatar of User"
+        />
+      </span>
+      Hi, User
+      <svg
+        class="h-3 ml-1 fill-current"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 20 20"
+      >
+        <path
+          d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"
+        />
+      </svg>
+    </button>
+    <div
+      id="userDropDown"
+      class="dropdownlist absolute bg-black text-white pin-r mt-3 p-3 overflow-auto z-30 invisible"
+    >
+      <a
+        href="#"
+        class="p-2 hover:bg-grey-darkest text-white text-sm no-underline hover:no-underline block"
+        ><i class="fa fa-user fa-fw"></i> Profile</a
+      >
+      <a
+        href="#"
+        class="p-2 hover:bg-grey-darkest text-white text-sm no-underline hover:no-underline block"
+        ><i class="fa fa-cog fa-fw"></i> Settings</a
+      >
+      <div class="border border-grey-darkest"></div>
+      <a
+        href="#"
+        class="p-2 hover:bg-grey-darkest text-white text-sm no-underline hover:no-underline block"
+        ><i class="fas fa-sign-out-alt fa-fw"></i> Log Out</a
+      >
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    window.onclick = event => {
+      if (
+        !event.target.matches('.drop-button') &&
+        !event.target.matches('.drop-search')
+      ) {
+        const dropdowns = document.getElementsByClassName('dropdownlist');
+        for (let i = 0; i < dropdowns.length; i++) {
+          const openDropdown = dropdowns[i];
+          if (!openDropdown.classList.contains('invisible')) {
+            openDropdown.classList.add('invisible');
+          }
+        }
+      }
+    };
+  },
+  methods: {
+    toggleDD(myDropMenu) {
+      document.getElementById(myDropMenu).classList.toggle('invisible');
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+#userDropDown {
+  min-width: 200px;
+}
+
+button {
+  display: flex;
+  align-items: center;
+}
+</style>
