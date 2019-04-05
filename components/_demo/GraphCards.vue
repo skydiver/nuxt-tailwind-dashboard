1684f705b990a365896ea08b89d499cea345bb10
Martín M <skydiver@users.noreply.github.com>
2019-04-04 21:37:03 -0300

    Table component

--- components/_demo/GraphCards.vue
+++ components/_demo/GraphCards.vue
@@ -4,6 +4,7 @@
     <Graph2 />
     <Graph3 />
     <Graph4 />
+    <Table />
   </div>
 </template>
 
@@ -12,13 +13,15 @@ import Graph1 from '~/components/_demo/Graph1.vue';
 import Graph2 from '~/components/_demo/Graph2.vue';
 import Graph3 from '~/components/_demo/Graph3.vue';
 import Graph4 from '~/components/_demo/Graph4.vue';
+import Table from '~/components/_demo/Table.vue';
 
 export default {
   components: {
     Graph1,
     Graph2,
     Graph3,
-    Graph4
+    Graph4,
+    Table
   }
 };
 </script>
---
+++ components/_demo/Table.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="w-full md:w-1/2 xl:w-1/3 p-3">
+    <Box title="Table">
+      <table class="w-full p-5 text-grey-darker">
+        <thead>
+          <tr>
+            <th class="text-left text-blue-darkest">Name</th>
+            <th class="text-left text-blue-darkest">Side</th>
+            <th class="text-left text-blue-darkest">Role</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Obi Wan Kenobi</td>
+            <td>Light</td>
+            <td>Jedi</td>
+          </tr>
+          <tr>
+            <td>Greedo</td>
+            <td>South</td>
+            <td>Scumbag</td>
+          </tr>
+          <tr>
+            <td>Darth Vader</td>
+            <td>Dark</td>
+            <td>Sith</td>
+          </tr>
+        </tbody>
+      </table>
+      <p class="py-2"><a href="#">See More issues...</a></p>
+    </Box>
+  </div>
+</template>
+
+<script>
+import Box from '~/components/containers/Box.vue';
+
+export default {
+  components: {
+    Box
+  }
+};
+</script>
