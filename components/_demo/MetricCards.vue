cc8394aa0ea534fde5df85cad1267367ca371b55
Martín M <skydiver@users.noreply.github.com>
2019-04-04 21:18:41 -0300

    added MetricCard component + demo

---
+++ components/_demo/MetricCards.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="flex flex-wrap">
+    <MetricCard
+      card-color="bg-green-lightest"
+      card-border-color="border-green-dark"
+      icon="fa-wallet"
+      icon-color="green-dark"
+      title="Total Revenue"
+      value="$3249"
+      arrow="up"
+      arrow-color="green"
+    />
+    <MetricCard
+      card-color="bg-orange-lightest"
+      card-border-color="border-orange"
+      icon="fa-users"
+      icon-color="orange-dark"
+      title="Total Users"
+      value="249"
+      arrow="equal"
+      arrow-color="orange"
+    />
+    <MetricCard
+      card-color="bg-yellow-lightest"
+      card-border-color="border-yellow-dark"
+      icon="fa-user-plus"
+      icon-color="yellow-dark"
+      title="New Users"
+      value="2"
+      arrow="up"
+      arrow-color="yellow-dark"
+    />
+
+    <MetricCard
+      card-color="bg-blue-lightest"
+      card-border-color="border-blue"
+      icon="fa-server"
+      icon-color="blue-dark"
+      title="Server Uptime"
+      value="152 days"
+    />
+    <MetricCard
+      card-color="bg-indigo-lightest"
+      card-border-color="border-indigo"
+      icon="fa-tasks"
+      icon-color="indigo-dark"
+      title="To Do List"
+      value="7 tasks"
+    />
+    <MetricCard
+      card-color="bg-red-lightest"
+      card-border-color="border-red"
+      icon="fa-inbox"
+      icon-color="red-dark"
+      title="Issues"
+      value="3"
+      arrow="up"
+      arrow-color="red"
+    />
+  </div>
+</template>
+
+<script>
+import MetricCard from '~/components/widgets/MetricCard.vue';
+
+export default {
+  components: {
+    MetricCard
+  }
+};
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,20 @@
 <template>
   <BaseContainer>
     <PageHeader title="HELLO WORLD!" />
+    <MetricCards />
   </BaseContainer>
 </template>
 
 <script>
 import BaseContainer from '~/components/containers/BaseContainer.vue';
 import PageHeader from '~/components/widgets/PageHeader.vue';
+import MetricCards from '~/components/_demo/MetricCards.vue';
 
 export default {
   components: {
     BaseContainer,
-    PageHeader
+    PageHeader,
+    MetricCards
   }
 };
 </script>
