37bb40af8df1bc008613db732c1c3947cd8a5917
Martín M <skydiver@users.noreply.github.com>
2019-04-04 22:14:38 -0300

    created buttons page

---
+++ pages/buttons.vue
@@ -0,0 +1,17 @@
+<template>
+  <BaseContainer>
+    <PageHeader title="Buttons" />
+  </BaseContainer>
+</template>
+
+<script>
+import BaseContainer from '~/components/containers/BaseContainer.vue';
+import PageHeader from '~/components/widgets/PageHeader.vue';
+
+export default {
+  components: {
+    BaseContainer,
+    PageHeader
+  }
+};
+</script>
